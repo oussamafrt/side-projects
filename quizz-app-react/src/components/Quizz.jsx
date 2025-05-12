4211dae7d4c2000be41b48dcbdcc667865738507
oussamafrt <oussama.ferhat01@gmail.com>
2025-05-12 15:33:45 +0200

    implement quiz logic with questions, timer, and game flow

--- quizz-app-react/src/components/Quizz.jsx
+++ quizz-app-react/src/components/Quizz.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../sounds/play.mp3";
+import correct from "../sounds/correct.mp3";
+import wrong from "../sounds/wrong.mp3";
 
 export default function Quizz({questions, setStop, questionNumber, setQuestionNumber}) {
     const [question, setQuestion] = useState(null);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
     const [className, setClassName] = useState("answer");
+    const [playSound] = useSound(play);
+    const [playCorrect] = useSound(correct);
+    const [playWrong] = useSound(wrong);
+
+    useEffect(() => {
+        playSound();
+    }, [playSound]);
 
     useEffect(() => {
         setQuestion(questions[questionNumber - 1])
@@ -21,13 +32,19 @@ export default function Quizz({questions, setStop, questionNumber, setQuestionNu
         delay(3000, () => 
             setClassName(a.isCorrect ? "answer correct" : "answer wrong")
         );
-        delay(6000, () => 
+        delay(5000, () => 
         {
             if (a.isCorrect){
-                setQuestionNumber((prev) => prev + 1);
-                setSelectedAnswer(null);
+                playCorrect();
+                delay(1000, ()=>{
+                    setQuestionNumber((prev) => prev + 1);
+                    setSelectedAnswer(null);
+                });
             } else {
-                setStop(true);
+                playWrong();
+                delay(1000, () => {
+                    setStop(true);
+                })
             }
         });
     }
